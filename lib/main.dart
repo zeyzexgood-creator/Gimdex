3dc3c2832c571a3acd604cd8a6f6f423a5c99bca
zeyzexgood-creator <zeyzexgood@gmail.com>
2026-06-01 12:13:36 +0800

    DevStudio IDE: Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Hack & Slash',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Stack(
-          children: [
-            GameWidget(game: HackSlashGame()),
-            Positioned(
-              top: 40,
-              left: 16,
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.7),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: const Text(
-                  '👆 Tap untuk serang | Geser untuk bergerak',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ],
-        ),
+        body: GameWidget(game: HackSlashGame()),
       ),
     );
   }
