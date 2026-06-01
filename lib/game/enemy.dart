841c7c99524e42ac3df20cbedb5862fd958f9076
zeyzexgood-creator <zeyzexgood@gmail.com>
2026-06-01 12:13:40 +0800

    DevStudio IDE: Update lib/game/enemy.dart

--- lib/game/enemy.dart
+++ lib/game/enemy.dart
@@ -3,36 +3,26 @@ import 'package:flutter/material.dart';
 
 class Enemy extends RectangleComponent {
   double health = 2;
-  final double speed = 80;
-  late double originalHealth;
-
-  Enemy({required Vector2 position}) : super(
+  final double speed = 70;
+  
+  Enemy() : super(
     size: Vector2(35, 35),
-    position: position,
     paint: Paint()..color = Colors.red,
     anchor: Anchor.center,
   );
 
   void takeDamage() {
     health--;
-    // Flash merah saat kena damage
     paint.color = Colors.orange;
     Future.delayed(const Duration(milliseconds: 100), () {
       if (health > 0) paint.color = Colors.red;
+      else paint.color = Colors.grey;
     });
   }
 
-  Rect get hitboxRect => Rect.fromCenter(
+  Rect get hitbox => Rect.fromCenter(
     center: Offset(position.x, position.y),
     width: size.x,
     height: size.y,
   );
-
-  @override
-  void update(double dt) {
-    // Bergerak ke arah player
-    final player = (parent as dynamic).gameRef.player;
-    final direction = (player.position - position).normalized();
-    position += direction * speed * dt;
-  }
 }
