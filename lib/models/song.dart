05e9eae3125ea709414586cb060c8ebe94e9626b
zeyzexgood-creator <zeyzexgood@gmail.com>
2026-06-11 06:49:55 +0800

    Create models/song.dart

---
+++ lib/models/song.dart
@@ -0,0 +1,13 @@
+class Song {
+  final String title;
+  final String artist;
+  final String url;
+  final String coverArt;
+
+  Song({
+    required this.title,
+    required this.artist,
+    required this.url,
+    required this.coverArt,
+  });
+}
